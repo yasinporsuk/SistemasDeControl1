4831df0f0dd201652298e7f9edc01b531b4872b6
yasinporsuk <yasinporsuk@gmail.com>
2024-01-11 21:49:57 +0100

    tercer commit

--- src/sistemasdeControl/ejercicioCompartido.java
+++ src/sistemasdeControl/ejercicioCompartido.java
@@ -1,5 +1,21 @@
 package sistemasdeControl;
 
+import java.util.Scanner;
+
 public class ejercicioCompartido {
+	
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner (System.in);
+		//el area del triangulo
+		System.out.println("Introduce la altura del triangulo: ");
+		int altura = entrada.nextInt();
+		System.out.println("Introduce la base del triangulo : ");
+		int base = entrada.nextInt();
+		System.out.println("el area del triangulo es : "+calcularArea(base,altura));
+		
+    }public static double calcularArea(int base, int altura) {
+	return base * altura / 2;
+	
+	
 
 }
