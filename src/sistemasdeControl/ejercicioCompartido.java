1016bbf06c111ba06b8c947e3b9e8e893c3fd8f8
Miha <Miha@DESKTOP-OPA0CCV.home>
2024-01-14 21:26:18 +0100

    area rectangulo

--- src/sistemasdeControl/ejercicioCompartido.java
+++ src/sistemasdeControl/ejercicioCompartido.java
@@ -22,5 +22,7 @@ public class ejercicioCompartido {
 }public static double perimetroACirc(int radio) {
 	double perimetro=2*(3.14*radio);
 	return perimetro;
+}public static double areaRectangulo(int lado1rec,int lado2rec) {
+	return ((lado1rec*2) + (lado2rec*2));
 }
 }
